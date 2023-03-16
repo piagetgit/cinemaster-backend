dc03b70e804dbe9b6cdf0c15eeaaadc652aa53a3
edoardopastori <127299734+edoardopastori@users.noreply.github.com>
2023-03-16 11:55:24 +0100

    Add files via upload
    corretta estensione file da .txt a .sql

---
+++ user-service/src/main/resources/db/insert_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO User (id,nome,cognome,password,mail,data_nascita,ruolo)
+VALUES (0001, 'Alice', 'Corvetto', 'alccrvtt', 'alice.corvetto@cmail.it', '1999-03-03', 'utente'),
+(0002, 'Marco', 'Donati', 'mrcdnt', 'marco.donati@cmail.it', '2001-05-05', 'utente'),
+(0003, 'Giovanni', 'Roselli', 'gvnnrsll', 'giovanni_roselli@cmail.it', '2000-04-04', 'utente'),
+(0004, 'Luca', 'Grazioli', 'cinemaster_77456', 'admin_luca@cinemaster.com', '1970-01-01', 'admin'),
+(0005, 'Sara', 'Flori', 'cinemaster_33245', 'admin_sara@cinemaster.it', '1970-01-01', 'admin');
