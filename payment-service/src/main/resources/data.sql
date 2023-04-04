b56ca4b67ad37ca9eb597ccb47ec31912ba20bc0
piagetgit <bouakapgt@gmail.com>
2023-04-04 11:45:27 +0200

    fix bug

--- payment-service/src/main/resources/data.sql
+++ payment-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO tickets (id,film_id,user_id,numero_persone,prezzo_totale,data_ora,numero_ridotti,posti,pagato)
 VALUES (20, 1, 20, 2, 30.0, '2023-04-04 15:00:00-02', 0,'A1,A2','0'),
-(21, 1,20, 1, 30.0, '2023-08-04 19:00:00-02', 0,'G5','1');
+(21, 2,20, 1, 30.0, '2023-08-04 19:00:00-02', 0,'G5','1');
